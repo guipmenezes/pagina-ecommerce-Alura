e05400fa6581d4d578ea5289529a050454e6d6e8
guipmenezes <guimenezesunb@gmail.com>
2022-08-13 23:35:02 -0300

    Fixing a new product bug

--- assets/javascript/controller/cadastraProdutos- controller.js
+++ assets/javascript/controller/cadastraProdutos- controller.js
@@ -4,11 +4,11 @@ const formulario = document.querySelector('[data-form]')
 
 formulario.addEventListener('submit', (evento) => {
     evento.preventDefault()
-    const image = document.querySelector('[data-image]').value
-    const nome = document.querySelector('[data-nome]').value
-    const preco = document.querySelector('[data-preco]').value
-    const section = document.querySelector('[data-category]').value
-    const description = document.querySelector('[data-desc]').value
+    const image = document.querySelector('[data-img]').value
+    const nome = document.querySelector('[data-name]').value
+    const preco = document.querySelector('[data-price]').value
+    const section = document.querySelector('[data-ctg]').value
+    const description = document.querySelector('[data-description]').value
 
     productService.criaProduto(nome, image, preco, section, description).then(resposta => {
         window.location.pathname = 'assets/html/all-products.html'
